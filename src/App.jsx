f45b7b761b1971acd42d03db703be295be1b0ecb
Shruti Mishra <smishra7272000@gmail.com>
2025-11-08 23:31:28 +0530

    finalCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
+import { useState } from 'react';
 
 function App() {
+
+    const [userInput,setUserInput]=useState({
+      initialInvestment: 0,
+      annualInvestment: 0,
+      expectedReturn: 0,
+      duration: 0
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+      setUserInput((prevUserInput) => {
+          return {
+              ...prevUserInput,
+              [inputIdentifier]: +newValue
+          };
+      });
+    }
+
+
   return (
+    <>
     <Header/>
-  )
+    <UserInput userInput={userInput} onChange={handleChange}/>
+    <Results input={userInput}/>
+    </>
+  );
 }
 
-export default App
+export default App;
